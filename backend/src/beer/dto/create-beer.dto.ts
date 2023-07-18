b000581e4d7889885f8e9d6beb7685f8ff08baec
Arielparicio28 <arielaparicio100@gmail.com>
2023-07-18 21:07:28 +0200

    Listo

---
+++ backend/src/beer/dto/create-beer.dto.ts
@@ -0,0 +1,10 @@
+
+
+export class CreateBeerDto{
+    image_url: string;
+      _id:string;
+      name: string;
+      tagline: string;
+      first_brewed: string
+      attenuation_level : number
+}
